bab0a96d79986184be89a1fe6802f05f3be603a5
dageon111 <dageon223@ewha.ac.kr>
2024-05-28 16:58:25 +0900

    Update ProfessorDAO.java
    db2024_StudentViewProfessor 뷰사용

--- src/student/ProfessorDAO.java
+++ src/student/ProfessorDAO.java
@@ -14,13 +14,13 @@ public class ProfessorDAO {
 
         switch (searchType) {//텍스트필드의 값의 속성에따라 달라지는 쿼리
             case "교수명":
-                query = "SELECT * FROM DB2024_Professor WHERE Name LIKE ?";//텍스트 필드의 정보가 이름일때
+                query = "SELECT * FROM db2024_StudentViewProfessor WHERE Name LIKE ?";//텍스트 필드의 정보가 이름일때
                 break;
             case "교수id":
-                query = "SELECT * FROM DB2024_Professor WHERE ProfessorID = ?";//텍스트 필드의 정보가 교수id일때
+                query = "SELECT * FROM db2024_StudentViewProfessor WHERE ProfessorID = ?";//텍스트 필드의 정보가 교수id일때
                 break;
             case "학과":
-                query = "SELECT * FROM DB2024_Professor WHERE Department LIKE ?";//텍스트 필드의 정보가 학과일때
+                query = "SELECT * FROM db2024_StudentViewProfessor WHERE Department LIKE ?";//텍스트 필드의 정보가 학과일때
                 break;
         }
 
